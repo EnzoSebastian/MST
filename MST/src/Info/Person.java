9f04c141cc252de0ef2e9f041a2f58f9047ddc0e
enzos <enzos@DESKTOP-N9SI45U>
2020-11-02 20:15:32 -0300

    Basic implementation of Classes Vertex and Person

--- MST/src/Info/Person.java
+++ MST/src/Info/Person.java
@@ -1,5 +1,24 @@
 package Info;
 
-public class Person {
+import java.util.HashMap;
 
+public class Person {
+	private String name;
+	private HashMap<String,Integer> interests;
+	
+	public Person(String name,int sports,int music,int newsShow,int science) {
+		this.name = name;
+		interests.put("sports", sports);
+		interests.put("music", music);
+		interests.put("newsShow", newsShow);
+		interests.put("science", science);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getInterest(String interest) {
+		return interests.get(interest);
+	}
 }
--- MST/src/Info/Vertex.java
+++ MST/src/Info/Vertex.java
@@ -1,5 +1,21 @@
 package Info;
 
-public class Vertex {
+import java.util.HashMap;
 
+public class Vertex {
+	private int value;
+	private HashMap<Vertex,Integer> edges;
+	
+	public Vertex(int value) {
+		this.value = value;
+		this.edges = new HashMap<>();
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public int getEdgeSimilarityIndex(Vertex v) {
+		return edges.get(v);
+	}
 }
