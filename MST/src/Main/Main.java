50dfea0b43fe7975fb4e2e7bb91731bcc52564ac
enzos <enzos@DESKTOP-N9SI45U>
2020-11-03 20:12:47 -0300

    Added Classes Interface and Main.

---
+++ MST/src/Main/Main.java
@@ -0,0 +1,23 @@
+package Main;
+
+import java.awt.EventQueue;
+
+import Interface.Interface;
+
+public class Main {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Interface frame = new Interface();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+	}
+
+}
